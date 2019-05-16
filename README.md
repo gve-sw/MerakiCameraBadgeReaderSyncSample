2a3c780b1a3a609f9a9dd22ddd413a7e0552876e
Gerardo Chaves <ggchaves@gmail.com>
2019-05-16 15:58:41 -0600

    code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,65 @@
-# MerakiCameraBadgeReaderSyncSample
+# Meraki Camera Badge Reader Sync Sample
 
-need to set the environment variable MERAKI_KEY with a Meraki API Key for the user with the right priviledges for the org: 
+This sample Flask application demonstrates how you could associate badge scanner event information
+with Meraki [Snapshot Network Camera API](https://developer.cisco.com/meraki/api/#/rest/api-endpoints/cameras/snapshot-network-camera) to allow a user to look back in time to particular timestamps and pull up
+the corresponding snapshot from the camera and alongside display a directory entry to help
+the operator validate that the person using the badge to enter a location is the one
+that appears in their direcotory photo. 
 
-export MERAKI_KEY="xxxxxxxxxxxxxxxxxxxxxxxxx"
+The sample badge reader events used in this example are contained in a List variable where 
+each element is a Dict with a timestamp and UserID for the ownder of the badge which we assume
+a badge entry solution can provide us.
+
+The data is defined as such in the app.py code file:
+
+```
+sampleBadgeReaderEvents=[{'timestamp': "2019-04-12T14:20:29-06:00", 'UserID': "85063715"},
+                         {'timestamp': "2019-04-12T11:40:33-06:00", 'UserID': "86543202"},
+                         {'timestamp': "2019-04-12T11:32:33-06:00", 'UserID': "86543201"},
+                         {'timestamp': "2019-04-12T11:33:36-06:00",'UserID': "85063685"}]
+```
+
+This sample uses a publicly available example directory application we built and are hosting in Heroku
+at http://sparkky.herokuapp.com. We are not documenting it's full usage since it is really
+meant as a placeholder for you to put in your own web based directory. You can put your own directory address in the 
+**screenshot.html** file in the templates directory. 
+
+### Requirements
+
+This sample depends on the following Python libraries:
+
+```
+Flask 
+render_template
+request
+jsonify
+make_response
+flask_httpauth
+flask_bcrypt
+simplepam
+time
+requests
+json
+```
+
+It also requires a **config.py** file in the same directory as the **app.py** file  with
+the following as it's content:
+
+```
+secret_key = 'SOME-KEY'
+meraki_key = 'THE-MERAKIAPI-KEY'
+meraki_network = 'MERAKI-NETWORK-ID'
+camera_serial = 'CAMERA-SERIAL'
+```
+
+### Running the sample
+
+Once you have entered the correct Meraki API key in the newly created **config.py** file, you should
+be able to start the Flash application by running the app.py script:
+
+```
+python3 app.py
+```
+
+Then open a browser and point to localhost port 5000 (https://0.0.0.0:5000)
+You will first be prompted to authenticate using credentials on the system you are executing the script from
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 # coding=utf-8
-from flask import Flask, render_template, request, session, jsonify, make_response, redirect as r
+"""
+Copyright (c) 2019 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+from flask import Flask, render_template, request, jsonify, make_response
 from flask_httpauth import HTTPBasicAuth
 from flask_bcrypt import Bcrypt
 from simplepam import authenticate
 import time
 import requests
 import json
-import os
 
 
 import config as c
@@ -20,7 +32,10 @@ app.secret_key = c.secret_key
 bcrypt = Bcrypt(app)
 auth = HTTPBasicAuth()
 
-latestMessagesList=[{'timestamp': "2019-04-12T14:20:29-06:00", 'UserID': "85063715"}, {'timestamp': "2019-04-12T11:40:33-06:00", 'UserID': "86543202"},{'timestamp': "2019-04-12T11:32:33-06:00", 'UserID': "86543201"},{'timestamp': "2019-04-12T11:33:36-06:00",'UserID': "85063685"}]
+sampleBadgeReaderEvents=[{'timestamp': "2019-04-12T14:20:29-06:00", 'UserID': "85063715"},
+                         {'timestamp': "2019-04-12T11:40:33-06:00", 'UserID': "86543202"},
+                         {'timestamp': "2019-04-12T11:32:33-06:00", 'UserID': "86543201"},
+                         {'timestamp': "2019-04-12T11:33:36-06:00",'UserID': "85063685"}]
 
 @auth.verify_password
 def verify_pw(username, password):
@@ -33,9 +48,9 @@ def unauthorized():
 @app.route('/')
 @auth.login_required
 def main():
-    global latestMessagesList
+    global sampleBadgeReaderEvents
     global i
-    return render_template('main.html',result=latestMessagesList)
+    return render_template('main.html',result=sampleBadgeReaderEvents)
 
 
 @app.route('/DoRetrieveSnapshot', methods=['POST'])
@@ -44,47 +59,35 @@ def doRetrieveSnapshot():
     primaryUserID=request.form['primaryUserID']
 
     #Here we will bring up another form to show snapshots for badge in events selected
-
-    print(request.form)
-    print(primaryTimeStamp)
-    print(primaryUserID)
-
-
-    url = "https://api.meraki.com/api/v0/networks/L_578149602163689741/cameras/Q2BV-7E5Q-ZZ2X/snapshot"
+    url = "https://api.meraki.com/api/v0/networks/"+c.meraki_network+"/cameras/"+c.camera_serial+"/snapshot"
 
     querystring = {"timestamp":primaryTimeStamp}
 
-    meraki_key = os.environ.get('MERAKI_KEY', None)
-
     payload = ""
     headers = {
-        'X-Cisco-Meraki-API-Key': meraki_key,
+        'X-Cisco-Meraki-API-Key': c.meraki_key,
         'cache-control': "no-cache",
-        'Postman-Token': "4b84ec0e-ed70-40fa-9603-fb56ce5f42d1"
     }
-
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
-    print(response.text)
+    print("response: ", response)
+    print("response.text: ", response.text)
     theURLDict = json.loads(response.text)
-    print(theURLDict["url"])
+    print("the URL: ",theURLDict["url"])
 
     #wait for the URL to be valid
     time.sleep(7)
 
     #Then return it to HTML
-#    return jsonify({'status': 'OK', 'value2': value2});
     return render_template('screenshot.html',result=[theURLDict["url"],primaryUserID])
 
 @app.route('/DoOK', methods=['POST'])
 def goBackMain():
     print(request.form)
-    global latestMessagesList
+    global sampleBadgeReaderEvents
 
     #Then return it to HTML
-#    return jsonify({'status': 'OK', 'value2': value2});
-    return render_template('main.html',result=latestMessagesList)
+    return render_template('main.html',result=sampleBadgeReaderEvents)
 
 if __name__ == "__main__":
 	#app.run(threaded=True)
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000,debug=True)
