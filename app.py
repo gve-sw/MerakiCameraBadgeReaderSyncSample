8e494c5cd74a1f0d9f0b0bd2cf04ba0ad4e35f84
Gerardo Chaves <ggchaves@gmail.com>
2020-05-15 13:28:55 -0600

    correcting license file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 """
-Copyright (c) 2019 Cisco and/or its affiliates.
+Copyright (c) 2020 Cisco and/or its affiliates.
+
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
 License at
+
                https://developer.cisco.com/docs/licenses
+
 All use of the material herein must be in accordance with the terms of
 the License. All rights not expressly granted by the License are
 reserved. Unless required by applicable law or agreed to separately in
